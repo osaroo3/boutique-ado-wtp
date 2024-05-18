62ff3b2eb0957bbb23393a68a3c1d42762c3d98f
Arukwe Obo-Eba <oboebafriday@gmail.com>
2024-05-18 04:11:18 +0000

    Updated context processor

--- bag/views.py
+++ bag/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, reverse, redirect
+from django.shortcuts import render, redirect
 
 # Create your views here.
 
@@ -21,5 +21,4 @@ def add_to_bag(request, item_id):
         bag[item_id] = quantity
 
     request.session['bag'] = bag
-    print(request.session['bag'])
     return redirect(redirect_url)
